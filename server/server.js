2e7a1ea82eb9190dc756ec57ee937977932920d0
$imba <107941962+sinaun4given@users.noreply.github.com>
2023-09-05 19:03:21 +0330

    init commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const path = require("path")
 const express = require("express")
+const socketIo = require("socket.io")
+const http = require ("http")
 var app = express()
+var server = http.createServer(app)
+var io = socketIo(server)
+
 var publicPath = path.join(__dirname,"../public/")
 var port = 3000
 
-
+io.on('connection',(socket)=>{
+    console.log("new user conected")
+    socket.on("disconnect",()=>{
+        console.log("user disconected")
+    })
+} )
 app.use(express.static(publicPath))
-app.listen(port,()=>{console.log(`server is on port ${port}`)})
+server.listen(port,()=>{console.log(`server is on port ${port}`)})
